77ae6edb8ed03305108b430d9fe2163391c94440
r01010010 <face.70.troll@gmail.com>
2024-01-10 15:10:51 +0700

    fix 0.1.2

--- lib/ui/widgets/widget_data/box_data.dart
+++ lib/ui/widgets/widget_data/box_data.dart
@@ -1,7 +1,7 @@
 part of '../../../ice_flutter_toolkit.dart';
 
 class BoxData {
-  const BoxData.box({
+  const BoxData._box({
     this.fillColor,
     required this.borderRadius,
     required this.radius,
@@ -10,38 +10,39 @@ class BoxData {
   });
 
   final Color? fillColor;
-  final BoxBorder border;
   final BorderRadius borderRadius;
   final Radius radius;
+  final BoxBorder? border;
   final BorderSide side;
+  //
+  // factory BoxData.r15All(
+  //         {double width = 2,
+  //         Color borderColor = const Color(0xFF858585),
+  //         Color? fillColor}) =>
+  //     BoxData.box(
+  //       fillColor: fillColor,
+  //       borderRadius: BorderRadius.circular(15),
+  //       radius: const Radius.circular(15),
+  //       side: BorderSide(color: borderColor, width: width),
+  //       border: Border.all(
+  //         width: width,
+  //         color: borderColor,
+  //       ),
+  //     );
 
-  factory BoxData.r15All(
-          {double width = 2,
-          Color borderColor = const Color(0xFF858585),
-          Color? fillColor}) =>
-      BoxData.box(
+  factory BoxData.radiusAll({
+    double radius = 20,
+    double width = 2,
+    Color? borderColor,
+    Color? fillColor,
+  }) =>
+      BoxData._box(
         fillColor: fillColor,
-        borderRadius: BorderRadius.circular(15),
-        radius: const Radius.circular(15),
-        side: BorderSide(color: borderColor, width: width),
-        border: Border.all(
-          width: width,
-          color: borderColor,
-        ),
-      );
-
-  factory BoxData.r20All(
-          {double width = 2,
-          Color borderColor = const Color(0xFF858585),
-          Color? fillColor}) =>
-      BoxData.box(
-        fillColor: fillColor,
-        borderRadius: BorderRadius.circular(20),
-        radius: const Radius.circular(20),
-        side: BorderSide(color: borderColor, width: width),
-        border: Border.all(
-          width: width,
-          color: borderColor,
-        ),
+        borderRadius: BorderRadius.circular(radius),
+        radius: Radius.circular(radius),
+        side: borderColor == null
+            ? BorderSide.none
+            : BorderSide(color: borderColor, width: width),
+        border: borderColor == null ? Border.all() : null,
       );
 }
